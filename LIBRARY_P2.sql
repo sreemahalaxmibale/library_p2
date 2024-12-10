cc36105cf07de541fa1149a11b19654f1a0e8cca
sreemahalaxmibale <68994970+sreemahalaxmibale@users.noreply.github.com>
2024-12-09 22:34:46 -0600

    Add files via upload

---
+++ LIBRARY_P2.sql
@@ -0,0 +1,358 @@
+SELECT * FROM books;
+SELECT * FROM branch;
+SELECT * FROM employees;
+SELECT * FROM issued_status;
+SELECT * FROM return_status;
+SELECT * FROM members;
+
+
+-- Project Task
+
+-- Task 1. Create a New Book Record -- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
+
+INSERT INTO books(isbn, book_title, category, rental_price, status, author, publisher)
+VALUES
+('978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.');
+SELECT * FROM books;
+
+
+-- Task 2: Update an Existing Member's Address
+
+UPDATE members
+SET member_address = '125 Main St'
+WHERE member_id = 'C101';
+SELECT * FROM members;
+
+
+-- Task 3: Delete a Record from the Issued Status Table 
+-- Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
+
+SELECT * FROM issued_status
+WHERE issued_id = 'IS121';
+
+DELETE FROM issued_status
+WHERE issued_id = 'IS121'
+
+
+
+-- Task 4: Retrieve All Books Issued by a Specific Employee -- Objective: Select all books issued by the employee with emp_id = 'E101'.
+
+SELECT * FROM issued_status
+WHERE issued_emp_id = 'E101';
+
+
+-- Task 5: List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
+
+
+
+SELECT 
+    ist.issued_emp_id,
+     e.emp_name
+    -- COUNT(*)
+FROM issued_status as ist
+JOIN
+employees as e
+ON e.emp_id = ist.issued_emp_id
+GROUP BY 1, 2
+HAVING COUNT(ist.issued_id) > 1
+
+
+-- CTAS
+-- Task 6: Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt**
+
+CREATE TABLE book_cnts
+AS    
+SELECT 
+    b.isbn,
+    b.book_title,
+    COUNT(ist.issued_id) as no_issued
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1, 2;
+
+
+SELECT * FROM
+book_cnts;
+
+
+
+-- Task 7. Retrieve All Books in a Specific Category:
+
+SELECT * FROM books
+WHERE category = 'Classic'
+
+    
+-- Task 8: Find Total Rental Income by Category:
+
+
+SELECT
+    b.category,
+    SUM(b.rental_price),
+    COUNT(*)
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1
+
+
+-- List Members Who Registered in the Last 180 Days:
+
+SELECT * FROM members
+WHERE reg_date >= CURRENT_DATE - INTERVAL '180 days'    
+    
+
+INSERT INTO members(member_id, member_name, member_address, reg_date)
+VALUES
+('C118', 'sam', '145 Main St', '2024-06-01'),
+('C119', 'john', '133 Main St', '2024-05-01');
+
+
+
+-- task 10 List Employees with Their Branch Manager's Name and their branch details:
+
+SELECT 
+    e1.*,
+    b.manager_id,
+    e2.emp_name as manager
+FROM employees as e1
+JOIN  
+branch as b
+ON b.branch_id = e1.branch_id
+JOIN
+employees as e2
+ON b.manager_id = e2.emp_id
+
+
+-- Task 11. Create a Table of Books with Rental Price Above a Certain Threshold 7USD:
+
+CREATE TABLE books_price_greater_than_seven
+AS    
+SELECT * FROM Books
+WHERE rental_price > 7
+
+SELECT * FROM 
+books_price_greater_than_seven
+
+
+-- Task 12: Retrieve the List of Books Not Yet Returned
+
+SELECT 
+    DISTINCT ist.issued_book_name
+FROM issued_status as ist
+LEFT JOIN
+return_status as rs
+ON ist.issued_id = rs.issued_id
+WHERE rs.return_id IS NULL
+
+    
+SELECT * FROM return_status
+--Task 13: Identify Members with Overdue Books
+--Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's_id, member's name, book title, issue date, and days overdue.
+
+
+SELECT 
+    ist.issued_member_id,
+    m.member_name,
+    bk.book_title,
+    ist.issued_date,
+    -- rs.return_date,
+    CURRENT_DATE - ist.issued_date as over_dues_days
+FROM issued_status as ist
+JOIN 
+members as m
+    ON m.member_id = ist.issued_member_id
+JOIN 
+book as bk
+ON bk.isbn = ist.issued_book_isbn
+LEFT JOIN 
+return_status as rs
+ON rs.issued_id = ist.issued_id
+WHERE 
+    rs.return_date IS NULL
+    AND
+    (CURRENT_DATE - ist.issued_date) > 30
+ORDER BY 1
+
+
+--Task 14: Update Book Status on Return
+--Write a query to update the status of books in the books table to "Yes" when they are returned (based on entries in the return_status table).
+
+
+CREATE OR REPLACE PROCEDURE add_return_records(p_return_id VARCHAR(10), p_issued_id VARCHAR(10), p_book_quality VARCHAR(10))
+LANGUAGE plpgsql
+AS $$
+
+DECLARE
+    v_isbn VARCHAR(50);
+    v_book_name VARCHAR(80);
+    
+BEGIN
+    -- all your logic and code
+    -- inserting into returns based on users input
+    INSERT INTO return_status(return_id, issued_id, return_date, book_quality)
+    VALUES
+    (p_return_id, p_issued_id, CURRENT_DATE, p_book_quality);
+
+    SELECT 
+        issued_book_isbn,
+        issued_book_name
+        INTO
+        v_isbn,
+        v_book_name
+    FROM issued_status
+    WHERE issued_id = p_issued_id;
+
+    UPDATE books
+    SET status = 'yes'
+    WHERE isbn = v_isbn;
+
+    RAISE NOTICE 'Thank you for returning the book: %', v_book_name;
+    
+END;
+$$
+
+
+-- Testing FUNCTION add_return_records
+
+issued_id = IS135
+ISBN = WHERE isbn = '978-0-307-58837-1'
+
+SELECT * FROM book
+WHERE isbn = '978-0-307-58837-1';
+
+SELECT * FROM issued_status
+WHERE issued_book_isbn = '978-0-307-58837-1';
+
+SELECT * FROM return_status
+WHERE issued_id = 'IS135';
+
+-- calling function 
+CALL add_return_records('RS138', 'IS135', 'Good');
+
+-- calling function 
+CALL add_return_records('RS148', 'IS140', 'Good');
+
+
+
+/*Task 15: Branch Performance Report
+Create a query that generates a performance report for each branch, showing the number of books issued, the number of books returned, and the total revenue generated from book rentals.
+*/
+
+CREATE TABLE branch_reports
+AS
+SELECT 
+    b.branch_id,
+    b.manager_id,
+    COUNT(ist.issued_id) as number_book_issued,
+    COUNT(rs.return_id) as number_of_book_return,
+    SUM(bk.rental_price) as total_revenue
+FROM issued_status as ist
+JOIN 
+employees as e
+ON e.emp_id = ist.issued_emp_id
+JOIN
+branch as b
+ON e.branch_id = b.branch_id
+LEFT JOIN
+return_status as rs
+ON rs.issued_id = ist.issued_id
+JOIN 
+book as bk
+ON ist.issued_book_isbn = bk.isbn
+GROUP BY 1, 2;
+
+SELECT * FROM branch_reports;
+
+
+
+--task16
+
+
+CREATE TABLE active_members
+AS
+SELECT * FROM members
+WHERE member_id IN (SELECT 
+                        DISTINCT issued_member_id   
+                    FROM issued_status
+                    WHERE 
+                        issued_date >= CURRENT_DATE - INTERVAL '2 month'
+                    )
+;
+
+SELECT * FROM active_members;
+
+
+--task17
+SELECT 
+    e.emp_name,
+    b.*,
+    COUNT(ist.issued_id) as no_book_issued
+FROM issued_status as ist
+JOIN
+employees as e
+ON e.emp_id = ist.issued_emp_id
+JOIN
+branch as b
+ON e.branch_id = b.branch_id
+GROUP BY 1, 2
+
+
+--task18
+
+CREATE OR REPLACE PROCEDURE issue_book(p_issued_id VARCHAR(10), p_issued_member_id VARCHAR(30), p_issued_book_isbn VARCHAR(30), p_issued_emp_id VARCHAR(10))
+LANGUAGE plpgsql
+AS $$
+
+DECLARE
+-- all the variabable
+    v_status VARCHAR(10);
+
+BEGIN
+-- all the code
+    -- checking if book is available 'yes'
+    SELECT 
+        status 
+        INTO
+        v_status
+    FROM books
+    WHERE isbn = p_issued_book_isbn;
+
+    IF v_status = 'yes' THEN
+
+        INSERT INTO issued_status(issued_id, issued_member_id, issued_date, issued_book_isbn, issued_emp_id)
+        VALUES
+        (p_issued_id, p_issued_member_id, CURRENT_DATE, p_issued_book_isbn, p_issued_emp_id);
+
+        UPDATE books
+            SET status = 'no'
+        WHERE isbn = p_issued_book_isbn;
+
+        RAISE NOTICE 'Book records added successfully for book isbn : %', p_issued_book_isbn;
+
+
+    ELSE
+        RAISE NOTICE 'Sorry to inform you the book you have requested is unavailable book_isbn: %', p_issued_book_isbn;
+    END IF;
+END;
+$$
+
+-- Testing The function
+SELECT * FROM books;
+-- "978-0-553-29698-2" -- yes
+-- "978-0-375-41398-8" -- no
+SELECT * FROM issued_status;
+
+CALL issue_book('IS155', 'C108', '978-0-553-29698-2', 'E104');
+CALL issue_book('IS156', 'C108', '978-0-375-41398-8', 'E104');
+
+SELECT * FROM books
+WHERE isbn = '978-0-375-41398-8'
+
+
+
+
+
+
